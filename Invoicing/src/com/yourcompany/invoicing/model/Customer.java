cfb626053eeb6efd0ff5b6cc6e02e49061bb4e0b
Diogo Paza <diogopazacvel@gmail.com>
2022-07-19 14:40:01 -0300

    finished chapter 4

--- Invoicing/src/com/yourcompany/invoicing/model/Customer.java
+++ Invoicing/src/com/yourcompany/invoicing/model/Customer.java
@@ -6,15 +6,17 @@ import org.openxava.annotations.*;
 
 import lombok.*;
 
-@Entity //diz que a classe é uma entidade
-@Getter @Setter //torna público todos os atributos
+@View(name = "Simple", members = "number, name")
+@Entity // diz que a classe é uma entidade
+@Getter
+@Setter // torna público todos os atributos
 public class Customer {
 	@Id
-	@Column(length=6)
+	@Column(length = 6)
 	int number;
-	
-	@Column(length=50)
-	@Required //campo obrigatório não pode ficar vazio
-	String name;	
+
+	@Column(length = 50)
+	@Required // campo obrigatório não pode ficar vazio
+	String name;
 
 }
