7931c6dc064f4c43829b94bd286cf6a5a0cfede4
Diogo Paza <diogopazacvel@gmail.com>
2022-07-22 10:38:09 -0300

    finished class 16

--- Invoicing/src/com/yourcompany/invoicing/model/Order.java
+++ Invoicing/src/com/yourcompany/invoicing/model/Order.java
@@ -1,18 +1,39 @@
 package com.yourcompany.invoicing.model;
 
+import java.time.*;
+
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
 
 import lombok.*;
 
-@View(extendsView = "super.DEFAULT", members = "invoice {invoice}")
+@View(extendsView = "super.DEFAULT", members = "estimatedDeliveryDays, invoice {invoice}")
 @View(name = "NoCustomerNoInvoice", members = "year,number,date;" + "details;" + "remarks")
 
 @Entity
 @Getter
 @Setter
 public class Order extends CommercialDocument {
+	
+	@Depends("date")
+	public int getEstimatedDeliveryDays() {
+		if(getDate().getDayOfYear() < 15) {
+			return 20 - getDate().getDayOfYear();
+		}
+		if(getDate().getDayOfWeek() == DayOfWeek.SUNDAY) return 2;
+		if(getDate().getDayOfWeek() == DayOfWeek.SATURDAY) return 3;
+		return 1;
+	}
+	
+	@PrePersist @PreUpdate
+	private void recalculateDeliveryDays() {
+		setDeliveryDays(getEstimatedDeliveryDays());
+	}
+	
+	@Column(columnDefinition="INTEGER DEFAULT 1")
+	int deliveryDays;
+	
 	@ManyToOne
 	@ReferenceView("NoCustomerNoOrders")
 	private Invoice invoice;
