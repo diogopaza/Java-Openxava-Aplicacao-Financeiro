863191de4a7bc264994bdac72026f01922e72af9
Diogo Paza <diogopazacvel@gmail.com>
2022-07-22 09:31:25 -0300

    finished class 15

--- Invoicing/src/com/yourcompany/invoicing/model/CommercialDocument.java
+++ Invoicing/src/com/yourcompany/invoicing/model/CommercialDocument.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
+import org.openxava.jpa.*;
 
 import com.yourcompany.invoicing.calculators.*;
 
@@ -21,12 +22,23 @@ import lombok.*;
 @Setter
 abstract public class CommercialDocument extends Identifiable {
 
+	@PrePersist
+	private void calculateNumber() throws Exception {
+		Query query = XPersistence.getManager()
+				.createQuery("select max(i.number) from " + getClass().getSimpleName() + " i where i.year = :year");
+		query.setParameter("year", year);
+		Integer lastNumber = (Integer) query.getSingleResult();
+		this.number = lastNumber == null ? 1 : lastNumber + 1;
+
+	}
+
 	@Column(length = 4)
 	@DefaultValueCalculator(CurrentYearCalculator.class) // traz o ano atual já inserido para o usuário
 	int year;
 
 	@Column(length = 6)
-	@DefaultValueCalculator(value = NextNumberForYearCalculator.class, properties = @PropertyValue(name = "year"))
+	//@DefaultValueCalculator(value = NextNumberForYearCalculator.class, properties = @PropertyValue(name = "year"))
+	@ReadOnly
 	int number;
 
 	@Required
@@ -43,19 +55,36 @@ abstract public class CommercialDocument extends Identifiable {
 	 * @ListProperties("product.number, product.description, quantity, pricePerUnit, "
 	 * + "valorTotal+[" + "commercialDocument.vatPercentage," +
 	 * "commercialDocument.vat," + "commercialDocument.totalAmount" + "]" )
-	 
+	 * 
+	 * @ElementCollection
+	 * 
+	 * @ListProperties("product.number, product.description, quantity, pricePerUnit, amount+["
+	 * + "commercialDocument.vatPercentage," + "commercialDocument.vat," +
+	 * "commercialDocument.totalAmount" + "]") Collection<Detail> details;
+	 */
+
 	@ElementCollection
-	@ListProperties("product.number, product.description, quantity, pricePerUnit, amount+["
-			+ "commercialDocument.vatPercentage," + "commercialDocument.vat," + "commercialDocument.totalAmount" + "]")
-	Collection<Detail> details;*/
-	
-	@ElementCollection
-    @ListProperties(
-        "product.number, product.description, quantity, pricePerUnit, " +
-        "amount+[commercialDocument.vatPercentage, commercialDocument.vat, commercialDocument.totalAmount ]" )
-    private Collection<Detail> details;
-/*
-	@Digits(integer = 2, fraction = 0)
+	@ListProperties("product.number, product.description, quantity, pricePerUnit, "
+			+ "amount+[commercialDocument.vatPercentage, commercialDocument.vat, commercialDocument.totalAmount ]")
+	private Collection<Detail> details;
+	/*
+	 * @Digits(integer = 2, fraction = 0) BigDecimal vatPercentage;
+	 * 
+	 * @ReadOnly
+	 * 
+	 * @Stereotype("MONEY")
+	 * 
+	 * @Calculation("sum(details.amount) * vatPercentage / 100") BigDecimal vat;
+	 * 
+	 * @ReadOnly
+	 * 
+	 * @Stereotype("MONEY")
+	 * 
+	 * @Calculation("sum(details.amount) + vat") BigDecimal totalAmount;
+	 */
+
+	@DefaultValueCalculator(VatPercentageCalculator.class)
+	@Digits(integer = 2, fraction = 0) // To indicate its size
 	BigDecimal vatPercentage;
 
 	@ReadOnly
@@ -66,21 +95,7 @@ abstract public class CommercialDocument extends Identifiable {
 	@ReadOnly
 	@Stereotype("MONEY")
 	@Calculation("sum(details.amount) + vat")
-	BigDecimal totalAmount;*/
-	
-	@DefaultValueCalculator(VatPercentageCalculator.class)
-	@Digits(integer=2, fraction=0) // To indicate its size
-	BigDecimal vatPercentage;
-	   
-	@ReadOnly
-	@Stereotype("MONEY")
-	@Calculation("sum(details.amount) * vatPercentage / 100")
-	BigDecimal vat;
-
-	@ReadOnly
-	@Stereotype("MONEY")
-	@Calculation("sum(details.amount) + vat")    
-	BigDecimal totalAmount;    
+	BigDecimal totalAmount;
 
 	@Stereotype("MEMO")
 	String remarks;
