d26e2759cb241a7260f83a3113f79a9a3a7e61c5
Diogo Paza <diogopazacvel@gmail.com>
2022-07-24 10:58:47 -0300

    finished class 22

--- Invoicing/src/com/yourcompany/invoicing/validators/ISBNValidator.java
+++ Invoicing/src/com/yourcompany/invoicing/validators/ISBNValidator.java
@@ -1,15 +1,17 @@
 package com.yourcompany.invoicing.validators;
 
 import javax.validation.*;
+import javax.ws.rs.client.*;
 
+import org.apache.commons.logging.*;
 import org.openxava.util.*;
 
 import com.yourcompany.invoicing.annotations.*;
 
 public class ISBNValidator implements ConstraintValidator<ISBN, Object> {
 
-	private static org.apache.commons.validator.routines.ISBNValidator validator = 
-			new org.apache.commons.validator.routines.ISBNValidator();
+	private static Log log = LogFactory.getLog(ISBNValidator.class);
+	private static org.apache.commons.validator.routines.ISBNValidator validator = new org.apache.commons.validator.routines.ISBNValidator();
 
 	public void initialize(ISBN isbn) {
 
@@ -18,6 +20,28 @@ public class ISBNValidator implements ConstraintValidator<ISBN, Object> {
 	public boolean isValid(Object value, ConstraintValidatorContext context) {
 		if (Is.empty(value))
 			return true;
-		return validator.isValid(value.toString());
+		if (!validator.isValid(value.toString()))
+			return false;
+		return isbnExists(value);
+	}
+
+	private boolean isbnExists(Object isbn) {
+		
+		try {
+			String response = ClientBuilder.newClient()
+					.target("http://openlibrary.org/")
+					.path("/api/books")
+					.queryParam("jscmd", "data")
+					.queryParam("format", "json")
+					.queryParam("bibkeys", "ISBN" + isbn)
+					.request()
+					.get(String.class);
+			return !response.equals("{}");
+		}catch(Exception ex) {
+			log.warn("Impossible to connect to openlibrary.org to validate the ISBN fails ", ex);
+			return false;
+		}
+		
+				
 	}
 }
